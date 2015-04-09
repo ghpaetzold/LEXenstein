9468ba810d959c231c759dbd0499de6d2b7544c3
Gustavo Henrique Paetzold <ghpaetzold@outlook.com>
2015-04-09 10:56:02 +0100

    Added POS tag selector

--- lexenstein/selectors.py
+++ lexenstein/selectors.py
@@ -4,6 +4,73 @@ from scipy.spatial.distance import cosine
 import nltk
 import numpy as np
 
+class POSTagSelector:
+
+	def selectCandidates(self, substitutions, victor_corpus):
+		"""
+		Selects which candidates can replace the target complex words in each instance of a VICTOR corpus.
+	
+		@param substitutions: A dictionary linking complex words to a set of candidate substitutions
+		Example: substitutions['perched'] = {'sat', 'roosted'}
+		@param victor_corpus: Path to a corpus in the VICTOR format.
+		For more information about the file's format, refer to the LEXenstein Manual.
+		@return: Returns a vector of size N, containing a set of selected substitutions for each instance in the VICTOR corpus.
+		"""
+		selected_substitutions = []				
+
+		lexf = open(victor_corpus)
+		for line in lexf:
+			data = line.strip().split('\t')
+			sent = data[0].strip()
+			target = data[1].strip()
+			head = int(data[2].strip())
+		
+			candidates = []
+			if target in substitutions.keys():
+				target_POS = self.getTargetPOS(sent, target, head)
+				candidates = substitutions[target]
+				candidates = getCandidatesWithSamePOS(candidates, target_POS)
+		
+			selected_substitutions.append(candidates)
+		lexf.close()
+		return selected_substitutions
+	
+	def getTargetPOS(sent, target, head):
+		pos_data = nltk.pos_tag(sent)
+		return pos_data[head][1]
+		
+	def getCandidatesWithSamePOS(candidates, pos):
+		result = set([])
+		for candidate in candidates:
+			pos_data = nltk.pos_tag(candidate)
+			if pos_data[0][1]==pos:
+				result.add(candidate)
+		return result
+	
+	def toVictorFormat(self, victor_corpus, substitutions, output_path, addTargetAsCandidate=False):
+		"""
+		Saves a set of selected substitutions in a file in VICTOR format.
+	
+		@param victor_corpus: Path to the corpus in the VICTOR format to which the substitutions were selected.
+		@param substitutions: The vector of substitutions selected for the VICTOR corpus.
+		@param output_path: The path in which to save the resulting VICTOR corpus.
+		@param addTargetAsCandidate: If True, adds the target complex word of each instance as a candidate substitution.
+		"""
+		o = open(output_path, 'w')
+		f = open(victor_corpus)
+		for subs in substitutions:
+			data = f.readline().strip().split('\t')
+			sentence = data[0].strip()
+			target = data[1].strip()
+			head = data[2].strip()
+			
+			newline = sentence + '\t' + target + '\t' + head + '\t'
+			for sub in subs:
+				newline += '0:'+sub + '\t'
+			o.write(newline.strip() + '\n')
+		f.close()
+		o.close()
+
 class VoidSelector:
 
 	def selectCandidates(self, substitutions, victor_corpus):
