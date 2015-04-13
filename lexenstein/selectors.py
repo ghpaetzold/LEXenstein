2ad856bfbcf18c21c4515735450c59eaf691672b
Gustavo Henrique Paetzold <ghpaetzold@outlook.com>
2015-04-13 18:44:30 +0100

    Fixed BS

--- lexenstein/selectors.py
+++ lexenstein/selectors.py
@@ -7,13 +7,16 @@ import os
 
 class BoundarySelector:
 
-	def __init__(self, boundary_ranker):
+	def __init__(self, boundary_ranker, temp_folder):
 		"""
 		Creates an instance of the BoundarySelector class.
 	
 		@param boundary_ranker: An instance of the BoundaryRanker class.
+		@param temp_folder: Folder in which to save temporary files.
+		All temporary files are removed after they are no longer in use.
 		"""
 		self.ranker = boundary_ranker
+		self.temp_folder = temp_folder
 		
 	def trainSelector(self, victor_corpus, positive_range, loss, penalty, alpha, l1_ratio, epsilon):
 		"""
@@ -34,9 +37,11 @@ class BoundarySelector:
 		@param epsilon: Acceptable error margin.
 		Recommended values: 0.0001, 0.001
 		"""
+		temp_victor = self.temp_folder + '/temp_victor.txt'
+		self.generateSSVictorCorpus(victor_corpus, temp_victor)
+		self.ranker.trainRanker(temp_victor, positive_range, loss, penalty, alpha, l1_ratio, epsilon)
+		os.system('rm ' + temp_victor)
 	
-		self.ranker.trainRanker(victor_corpus, positive_range, loss, penalty, alpha, l1_ratio, epsilon)
-		
 	def trainSelectorWithCrossValidation(self, victor_corpus, positive_range, folds, test_size, losses=['hinge', 'modified_huber'], penalties=['elasticnet'], alphas=[0.0001, 0.001, 0.01], l1_ratios=[0.0, 0.15, 0.25, 0.5, 0.75, 1.0]):
 		"""
 		Trains a Boundary Selector while maximizing hyper-parameters through cross-validation.
@@ -60,9 +65,27 @@ class BoundarySelector:
 		@param epsilons: Acceptable error margins.
 		Recommended values: 0.0001, 0.001
 		"""
-		self.ranker.trainRankerWithCrossValidation(victor_corpus, positive_range, folds, test_size, losses=losses, penalties=penalties, alphas=alphas, l1_ratios=l1_ratios)
+		temp_victor = self.temp_folder + '/temp_victor.txt'
+		self.generateSSVictorCorpus(victor_corpus, temp_victor)
+		self.ranker.trainRankerWithCrossValidation(temp_victor, positive_range, folds, test_size, losses=losses, penalties=penalties, alphas=alphas, l1_ratios=l1_ratios)
+		os.system('rm ' + temp_victor)
 
-	def selectCandidates(self, substitutions, victor_corpus, temp_file, proportion):
+	def generateSSVictorCorpus(victor_corpus, temp_victor):
+		f = open(victor_corpus)
+		o = open(temp_victor, 'w')
+		for line in f:
+			data = line.strip().split('\t')
+			newline = data[0].strip() + '\t' + data[1].strip() + '\t' + data[2].strip() + '\t' + '1:'+data[1].strip() + '\t'
+			for subst in data[3:len(data)]:
+				substd = subst.strip().split(':')
+				rank = int(substd[0].strip())
+				word = substd[1].strip()
+				newline += str(rank+1)+':'+word + '\t'
+			o.write(newline.strip() + '\n')
+		f.close()
+		o.close()
+		
+	def selectCandidates(self, substitutions, victor_corpus, proportion):
 		"""
 		Selects which candidates can replace the target complex words in each instance of a VICTOR corpus.
 	
@@ -70,11 +93,11 @@ class BoundarySelector:
 		Example: substitutions['perched'] = {'sat', 'roosted'}
 		@param victor_corpus: Path to a corpus in the VICTOR format.
 		For more information about the file's format, refer to the LEXenstein Manual.
-		@param temp_file: File in which to save a temporary VICTOR corpus.
 		User must have the privilege to delete such file without administrator privileges.
 		@param proportion: Percentage of substitutions to keep.
 		@return: Returns a vector of size N, containing a set of selected substitutions for each instance in the VICTOR corpus.
 		"""
+		temp_file = self.temp_folder + '/temp_file.txt'
 		
 		void = VoidSelector()
 		selected_void = void.selectCandidates(substitutions, victor_corpus)
