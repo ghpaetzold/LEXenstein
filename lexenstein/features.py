a51500036338fa36a1b0ccf8c7896f5f10eac607
Gustavo Henrique Paetzold <ghpaetzold@outlook.com>
2015-08-17 17:04:27 +0100

    Added tagged word vector context similarity feature

--- lexenstein/features.py
+++ lexenstein/features.py
@@ -1486,6 +1486,71 @@ class FeatureEstimator:
 				result.append(similarity)
 		return result
 		
+	def taggedWordVectorContextSimilarityFeature(self, data, args):
+		model = self.resources[args[0]]
+		tagger = self.resources[args[1]]
+		pos_type = args[2]
+		result = []
+		
+		#Get tagged sentences:
+		tagged_sents = None
+		if 'tagged_sents' in self.temp_resources:
+			tagged_sents = self.temp_resources['tagged_sents']
+		else:
+			sentences = [l[0].strip().split(' ') for l in data]
+			tagged_sents = tagger.tag_sents(sentences)
+			self.temp_resources['tagged_sents'] = tagged_sents
+			
+		
+		#Produce embeddings vector tags:
+		model_tagged_sents = None
+		if pos_type=='paetzold':
+			transformed = []
+			for sent in tagged_sents:
+				tokens = []
+				for token in sent:
+					tokens.append((token[0], getGeneralisedPOS(token[1])))
+				transformed.append(tokens)
+			model_tagged_sents = transformed
+		else:
+			model_tagged_sents = tagged_sents
+			
+		for i in range(0, len(data)):
+			line = data[i]
+			tokens = line[0].strip().split(' ')
+			target = line[1].strip().lower()
+			head = int(line[2].strip())
+			target_pos = model_tagged_sents[i][head][1]
+			
+			#Get content words in sentence:
+			content_words = set([])
+			for j in range(0, len(tokens)):
+				token = tokens[j]
+				tag = tagged_sents[i][j][1]
+				model_tag = model_tagged_sents[i][j][1]
+				if self.isContentWord(token, tag):
+					content_words.add(token+'|||'+model_tag)
+					
+			print('Content words: ' + str(content_words))
+			
+			#Produce divisor:
+			divisor = float(len(content_words))
+			
+			for subst in line[3:len(line)]:
+				word = subst.strip().split(':')[1].strip()
+				similarity = 0.0
+				for content_word in content_words:
+					try:
+						similarity += model.similarity(content_word, word+'|||'+target_pos)
+					except KeyError:
+						try:
+							similarity += model.similarity(content_word, word.lower()+'|||'+target_pos)
+						except KeyError:
+							pass
+				similarity /= divisor
+				result.append(similarity)
+		return result
+		
 	def readNgramFile(self, ngram_file):
 		counts = shelve.open(ngram_file, protocol=pickle.HIGHEST_PROTOCOL)
 		return counts
@@ -2416,3 +2481,35 @@ class FeatureEstimator:
 				self.resources[pos_model] = tagger
 			self.features.append((self.wordVectorContextSimilarityFeature, [model, pos_model]))
 			self.identifiers.append(('Word Vector Context Similarity (Model: '+model+') (POS Model: '+pos_model+')', orientation))
+
+	def addTaggedWordVectorContextSimilarityFeature(self, model, pos_model, stanford_tagger, java_path, pos_type, orientation):
+		"""
+		Adds a word vector context similarity feature to the estimator.
+		The value will be the average similarity between the word vector of a candidate and the vectors of all content word in the target word's context.
+	
+		@param model: Path to a binary word vector model.
+		For instructions on how to create the model, please refer to the LEXenstein Manual.
+		@param pos_model: Path to a POS tagging model for the Stanford POS Tagger.
+		The models can be downloaded from the following link: http://nlp.stanford.edu/software/tagger.shtml
+		@param stanford_tagger: Path to the "stanford-postagger.jar" file.
+		The tagger can be downloaded from the following link: http://nlp.stanford.edu/software/tagger.shtml
+		@param java_path: Path to the system's "java" executable.
+		Can be commonly found in "/usr/bin/java" in Unix/Linux systems, or in "C:/Program Files/Java/jdk_version/java.exe" in Windows systems.
+		@param pos_type: The type of POS tags to be used.
+		Values supported: treebank, paetzold
+		@param orientation: Whether the feature is a simplicity of complexity measure.
+		Possible values: Complexity, Simplicity.
+		"""
+		
+		if orientation not in ['Complexity', 'Simplicity']:
+			print('Orientation must be Complexity or Simplicity')
+		else:
+			if model not in self.resources:
+				m = gensim.models.word2vec.Word2Vec.load_word2vec_format(model, binary=True)
+				self.resources[model] = m
+			os.environ['JAVAHOME'] = java_path
+			if pos_model not in self.resources:
+				tagger = StanfordPOSTagger(pos_model, stanford_tagger)
+				self.resources[pos_model] = tagger
+			self.features.append((self.taggedWordVectorContextSimilarityFeature, [model, pos_model, pos_type]))
+			self.identifiers.append(('Tagged Word Vector Context Similarity (Model: '+model+') (POS Model: '+pos_model+') (POS Type: '+pos_type+')', orientation))
