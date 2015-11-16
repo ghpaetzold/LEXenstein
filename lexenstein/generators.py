039e64dbc9aecc4b90f8f2b6cf499cc0eb220398
Gustavo Henrique Paetzold <ghpaetzold@outlook.com>
2015-11-16 17:17:42 +0000

    Fixed Kauchak generator

--- lexenstein/generators.py
+++ lexenstein/generators.py
@@ -477,7 +477,7 @@ class KauchakGenerator:
 							if leftp in substitutions_initial[leftw]:
 								substitutions_initial[leftw][leftp].add(rightw)
 							else:
-								substitutions_initial[leftw][leftp] = set(rightw)
+								substitutions_initial[leftw][leftp] = set([rightw])
 						else:
 							substitutions_initial[leftw] = {leftp:set([rightw])}
 		fparallel.close()
